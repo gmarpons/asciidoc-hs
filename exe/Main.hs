ca65e915232a390c7df1e0e984e466ae2f57a745
Guillem Marpons <gmarpons@mailbox.org>
2020-06-22 11:15:44 +0200

    Init cabal project (lib + exe)

---
+++ exe/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import qualified Text.Asciidoc.Inlines as Inlines
+
+main :: IO ()
+main = do
+  putStrLn "Hello, Haskell!"
+  Inlines.someFunc
---
+++ src/Text/Asciidoc/Inlines.hs
@@ -0,0 +1,7 @@
+module Text.Asciidoc.Inlines
+  ( someFunc,
+  )
+where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
