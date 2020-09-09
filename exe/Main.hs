5aca9d83f934e96c3ee0a52d311c17f4c8793ca3
Guillem Marpons <gmarpons@mailbox.org>
2020-09-09 11:20:12 +0200

    Use getContents from text to read from stdin

--- exe/Main.hs
+++ exe/Main.hs
@@ -4,16 +4,16 @@ module Main
 where
 
 import qualified Data.Aeson.Text as Aeson
-import qualified Data.Text as T
-import qualified Data.Text.Lazy.IO as T
+import qualified Data.Text.IO as T
+import qualified Data.Text.Lazy.IO as LT
 import Text.AsciiDoc.Inlines
 import Text.AsciiDoc.Pandoc
 import qualified Text.Parsec as Parsec (runParser)
 
 main :: IO ()
 main = do
-  result <- Parsec.runParser (pInlines) initialState "" . T.pack <$> getContents
+  result <- Parsec.runParser (pInlines) initialState "" <$> T.getContents
   case result of
     Left err -> error $ "Parsing error: " <> show err
     Right inline ->
-      T.putStrLn $ Aeson.encodeToLazyText $ convertDocument [addSourcePositions inline]
+      LT.putStrLn $ Aeson.encodeToLazyText $ convertDocument [addSourcePositions inline]
