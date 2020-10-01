e10a5cb674a7167c38fa421613de4f5bd458152a
Guillem Marpons <gmarpons@mailbox.org>
2020-10-01 18:58:00 +0200

    Modify AST to attach metadata to each block
    Two new concepts are needed: BlockPrefixItem and DanglingBlockPrefix.
    
    The parser is more complex, but now some of the non-context-free aspects of Asciidoc (e.g. [discrete] section headers, different kinds of open blocks) can be handled by the parser itself.

--- src/Text/AsciiDoc/Blocks.hs
+++ src/Text/AsciiDoc/Blocks.hs
@@ -75,6 +75,7 @@ import Control.Monad.Combinators hiding
 import Control.Monad.Combinators.NonEmpty
 import Data.Char (isSpace)
 import Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as NE
 import qualified Data.Map as Map
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -82,7 +83,6 @@ import qualified Data.Text.IO as T
 import Text.AsciiDoc.Inlines hiding (parseTest)
 import qualified Text.AsciiDoc.LineParsers as LP
 import qualified Text.Parsec as Parsec
-import Text.Parsec.Pos (SourcePos)
 import Text.Parsec.Char (alphaNum, anyChar, char, satisfy, space)
 
 -- | An explicit header level is necessary, as the output style (e.g. font size)
@@ -192,42 +192,46 @@ data MetadataItem a
     BlockAttributeList [Text]
   deriving (Eq, Show, Functor)
 
+data BlockPrefixItem a
+  = MetadataItem (MetadataItem a)
+  | -- | A value of @Nothing@ means the attribute has been unset.
+    AttributeEntry AttributeId (Maybe Inline)
+  | Comment Comment
+  deriving (Eq, Show, Functor)
+
 -- | A Block consists, syntactically, of one or more contiguous and complete
 -- lines of text. Some block types can contain other blocks.
 data Block a
   = -- | Regular paragraph.
-    Paragraph a
+    Paragraph [BlockPrefixItem UnparsedInline] a
   | -- | This data constructor is not used during parsing, it requires an
     -- additional "nesting" pass.
     --
     -- There can be a @Section@ inside an, e.g., open block, but it needs to
     -- have the "discrete" attribute.
-    Section (SectionHeader a) [Block a]
+    Section [BlockPrefixItem UnparsedInline] (SectionHeader a) [Block a]
   | -- |
-    SectionHeaderBlock (SectionHeader a)
-  | List ListType (NonEmpty (NonEmpty (Block a)))
+    SectionHeaderBlock [BlockPrefixItem UnparsedInline] (SectionHeader a)
+  | List ListType [BlockPrefixItem UnparsedInline] (NonEmpty (NonEmpty (Block a)))
   | Table {- TODO. Many things here -}
-  | ThematicBreak
-  | PageBreak
+  | ThematicBreak [BlockPrefixItem UnparsedInline]
+  | PageBreak [BlockPrefixItem UnparsedInline]
   | -- | Sequence of blocks of some defined type that allows nested blocks
     -- inside (i.e. admonition, sidebar, example, quote, and open block with no
     -- other standard type).
-    Nestable NestableBlockType {-State-} [Block a {-State-}]
-  | VerseBlock [a]
+    Nestable NestableBlockType [BlockPrefixItem UnparsedInline {-State-}] [Block a] -- State
+  | VerseBlock [BlockPrefixItem UnparsedInline] [a]
   | -- | Block type determines substitution group applied: @Verbatim@ or @None@
     -- (aka passthrough).
     --
     -- TODO: Check that designed pipeline guarantees that pre-processor
     -- directives are expanded (if not escaped) even in literal blocks, as
     -- https://asciidoctor.org/docs/user-manual/#include-processing states.
-    LiteralBlock LiteralBlockType [Text]
+    LiteralBlock LiteralBlockType [BlockPrefixItem UnparsedInline] [Text]
   | -- | Some macros accept block metadata, as e.g. @toc::[]@, that accepts
     -- defining its title with @.TITLE@ syntax.
-    BlockMacro BlockMacroType MacroArguments
-  | -- | A value of @Nothing@ means the attribute has been unset.
-    AttributeEntry AttributeId (Maybe Inline)
-  | Comment Comment
-  | MetadataItem (MetadataItem a)
+    BlockMacro BlockMacroType [BlockPrefixItem UnparsedInline] MacroArguments
+  | DanglingBlockPrefix [BlockPrefixItem UnparsedInline]
   deriving (Eq, Show, Functor)
 
 type UnparsedInline = NonEmpty UnparsedLine
@@ -266,21 +270,32 @@ pBlocks :: Parser [Block UnparsedInline]
 pBlocks = many pBlock
 
 pBlock :: Parser (Block UnparsedInline)
-pBlock =
-  Comment <$> pBlockComment
-    <|> Comment <$> pLineCommentSequence
-    <|> pAttributeEntry
-    <|> pBlockTitle
-    <|> pNestable
-    <|> Parsec.parserTraced "pSectionHeader" pSectionHeader
-    <|> Parsec.parserTraced "pParagraph" pParagraph
+pBlock = do
+  prefix <- option [] (NE.toList <$> pBlockPrefix)
+  pBlock' prefix
+  where
+    pBlock' prefix =
+      pNestable prefix
+        <|> pSectionHeader prefix
+        <|> pParagraph prefix
+        <|> pDanglingBlockPrefix prefix
+
+pBlockPrefix :: Parser (NonEmpty (BlockPrefixItem UnparsedInline))
+pBlockPrefix = some pBlockPrefixItem
+  where
+    pBlockPrefixItem =
+      Comment <$> pBlockComment
+        <|> Comment <$> pLineCommentSequence
+        <|> pAttributeEntry
+        <|> pBlockTitle
 
 pBlockComment :: Parser Comment
 pBlockComment = do
   delimiter <- choice $ fmap pLine' $ LP.runOfN 4 ['/']
   let n = T.length delimiter
-  -- It's paramount to use here an alternative version of pLine, called pLine',
-  -- that does not try to handle pre-processor directives.
+  -- We use here an alternative version of pLine, called pLine', that does not
+  -- try to handle pre-processor directives, as includes have no effect inside
+  -- block comments.
   ts <- manyTill (pLine' LP.remaining) $ eitherP (pLine' (LP.count n (char '/'))) Parsec.eof
   option () pInclude
   _ <- many pBlankLine
@@ -296,7 +311,7 @@ pLineComment =
   pLine (LP.string "//" *> Parsec.notFollowedBy (char '/') *> LP.remaining)
 
 -- TODO. Add attribute continuations.
-pAttributeEntry :: Parser (Block a)
+pAttributeEntry :: Parser (BlockPrefixItem a)
 pAttributeEntry = pAttributeEntry' <* Parsec.try (many pBlankLine)
   where
     pAttributeEntry' = do
@@ -312,7 +327,7 @@ pAttributeEntry = pAttributeEntry' <* Parsec.try (many pBlankLine)
       Parsec.modifyState $ \st -> st {env = Map.insert k v' (env st)}
       pure $ AttributeEntry k $ Just (parseInline' v)
 
-pBlockTitle :: Parser (Block UnparsedInline)
+pBlockTitle :: Parser (BlockPrefixItem UnparsedInline)
 pBlockTitle = pBlockTitle' <* many pBlankLine
   where
     pBlockTitle' =
@@ -320,29 +335,31 @@ pBlockTitle = pBlockTitle' <* many pBlankLine
         <$> pLine (LP.string "." *> (LP.satisfy (not . isSpace) <> LP.remaining))
 
 -- | Parses a nestable delimited block.
-pNestable :: Parser (Block UnparsedInline)
-pNestable = do
+pNestable :: [BlockPrefixItem UnparsedInline] -> Parser (Block UnparsedInline)
+pNestable prefix = do
   {-st1 <- Parsec.getState-}
-  c <- pOpenDelimiter ['=', '*']
+  delimiter <- pOpenDelimiter ['=', '*']
   {-st2 <- Parsec.getState-}
   bs <- manyTill pBlock $ eitherP pCloseDelimiter Parsec.eof
   _ <- many pBlankLine
-  pure $ case c of
-    '=' -> Nestable Example {-st1-} bs {-st2-}
-    '*' -> Nestable Sidebar {-st1-} bs {-st2-}
+  pure $ case delimiter of
+    '=' -> Nestable Example prefix {-st1-} bs {-st2-}
+    '*' -> Nestable Sidebar prefix {-st1-} bs {-st2-}
     x -> error $ "pNestable: unexpected character '" <> show x <> "'"
 
 -- | Parses a section header and computes its level.
 --
 -- POST-CONDITION: The computed level is greater or equal to 0. This follows
 -- from the fact that 'LP.runOfN 1' can only return texts of length >= 1.
-pSectionHeader :: Parser (Block UnparsedInline)
-pSectionHeader = pSectionHeader' <* many pBlankLine
+pSectionHeader ::
+  [BlockPrefixItem UnparsedInline] ->
+  Parser (Block UnparsedInline)
+pSectionHeader prefix =
+  SectionHeaderBlock <$> pure prefix <*> pSectionHeader' <* many pBlankLine
   where
     pSectionHeader' =
-      ( \(prefix, value) ->
-          SectionHeaderBlock $
-            SectionHeader (TextLine value :| []) (-1 + T.length prefix)
+      ( \(marker, value) ->
+          SectionHeader (TextLine value :| []) (-1 + T.length marker)
       )
         <$> pLine
           ( (,)
@@ -350,8 +367,9 @@ pSectionHeader = pSectionHeader' <* many pBlankLine
                 <*> (LP.satisfy (not . isSpace) <> LP.remaining)
           )
 
-pParagraph :: Parser (Block UnparsedInline)
-pParagraph = Paragraph <$> pParagraph' <* many pBlankLine
+pParagraph :: [BlockPrefixItem UnparsedInline] -> Parser (Block UnparsedInline)
+pParagraph prefix =
+  Paragraph <$> pure prefix <*> pParagraph' <* many pBlankLine
   where
     pParagraph' =
       (:|) <$> pFirst <*> many pFollowing
@@ -359,9 +377,13 @@ pParagraph = Paragraph <$> pParagraph' <* many pBlankLine
     pFirst =
       TextLine
         <$> pLineNoneOf
-          [ -- Blank line
-            mempty :: LP.LineParser Text
-          ]
+          -- Nestable
+          ( LP.runOfN 4 ['=', '*']
+              <> [
+                   -- Blank line
+                   mempty :: LP.LineParser Text
+                 ]
+          )
     -- Line comments (but not block comments!) can be contained in a paragraph.
     pFollowing =
       CommentLine <$> pLineComment
@@ -376,6 +398,14 @@ pParagraph = Paragraph <$> pParagraph' <* many pBlankLine
                      between (char '[') (char ']') (LP.many anyChar)
                    ]
             )
+
+pDanglingBlockPrefix ::
+  [BlockPrefixItem UnparsedInline] ->
+  Parser (Block UnparsedInline)
+pDanglingBlockPrefix [] = empty
+pDanglingBlockPrefix prefix =
+  DanglingBlockPrefix prefix
+    <$ Parsec.lookAhead (pCloseDelimiter <|> Parsec.eof)
 
 pInitialBlankLines :: Parser [Text]
 pInitialBlankLines = many pBlankLine
@@ -447,12 +477,25 @@ pInclude = do
 
 pOpenDelimiter :: [Char] -> Parser Char
 pOpenDelimiter cs = do
+  -- Parsec.lookAhead needed here because in case we fail later on(because the
+  -- block is already open) we don't want to consume any input.
+  t <- Parsec.lookAhead $ Parsec.try $ pLineOneOf (LP.runOfN 4 cs)
   -- WARNING! Use of PARTIAL FUNCTION 'T.head': 't' is guaranteed not to be
   -- empty because 'LP.runOfN 4' can only return texts with length >= 4.
-  t <- pLineOneOf (LP.runOfN 4 cs) <* many pBlankLine
   let (n, c) = (T.length t, T.head t)
-  Parsec.modifyState (\st -> st {openBlocks = (n, c) : openBlocks st})
-  pure c
+  st <- Parsec.getState
+  -- If block is already open (the delimiter is in the stack of open blocks),
+  -- we're not opening it again, but fail. In case we don't fail, we consume the
+  -- line that was looked ahead above.
+  case ((n, c) `elem` openBlocks st) of
+    True -> empty
+    False -> do
+      Parsec.putState (st {openBlocks = (n, c) : openBlocks st})
+      -- Consume one token (aka one line of input), and following blanklines
+      _ <- pLine $ LP.remaining
+      _ <- many pBlankLine
+      -- satisfyToken (const $ Just ())
+      pure c
 
 pCloseDelimiter :: Parser ()
 pCloseDelimiter = do
@@ -466,7 +509,9 @@ pCloseDelimiter = do
         pLine (LP.count n (char c))
           <|> Parsec.lookAhead (choice $ fmap (\(n', c') -> pLine' (LP.count n' (char c'))) bs)
       Parsec.putState $ st {openBlocks = drop 1 (openBlocks st)}
-    [] -> error "pCloseDelimiter: Empty State.openBlocks"
+    -- In presence of DanglingBlockPrefix'es, we can try to pop from an empty
+    -- openBlocks stack
+    [] -> pure ()
 
 -- TODO: Add name to source positions (possibly storing current filename when an
 -- inline arrives).
@@ -475,7 +520,7 @@ pCloseDelimiter = do
 satisfyToken :: (Text -> Maybe a) -> Parser a
 satisfyToken matcher = Parsec.tokenPrim show updatePos matcher
   where
-    updatePos :: SourcePos -> Text -> [Text] -> SourcePos
+    updatePos :: Parsec.SourcePos -> Text -> [Text] -> Parsec.SourcePos
     updatePos pos _ _ = Parsec.incSourceLine pos 1
 
 parseTest :: Parser a -> [Text] -> IO (Either Parsec.ParseError a)
@@ -487,7 +532,9 @@ readTokens file = do
   t <- T.readFile file
   pure $ T.lines t
 
-parseFile :: FilePath -> IO (Either Parsec.ParseError [Block UnparsedInline])
+parseFile ::
+  FilePath ->
+  IO (Either Parsec.ParseError [Block UnparsedInline])
 parseFile file = do
   tokens <- readTokens file
   parseTest pDocument tokens
